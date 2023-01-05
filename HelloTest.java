afefb19e5d1e94a471dd0c19640acedbc1f25757
PierreWena <h20133173@gmail.com>
2023-01-06 01:12:17 +0900

    message

---
+++ HelloTest.java
@@ -0,0 +1,21 @@
+package my.day01;
+
+//import java.util.Date;
+
+//import java.lang.*;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		System.out.println("Hello World");
+		System.out.println("안녕하세요");
+
+		java.util.Date now = new java.util.Date();
+		//Date now = new Date(); // import 해야 할 경우
+		System.out.println("현재시각 : " + now);
+		
+
+	}
+
+}
